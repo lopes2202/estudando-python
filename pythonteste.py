418fe54ca6c388005b3d5a15663bdbbee425b789
Gabriel Lopes <gabriel.ldbrito@gmail.com>
2024-02-28 18:40:04 -0300

    aprendendo mais

---
+++ pythonteste.py
@@ -0,0 +1,9 @@
+a = input('Digite algo: ')
+
+print("É um numero?", a.isnumeric())
+print("Faz parte do alfabeto", a.isalpha())
+print("Tem letra minuscula", a.islower())
+print("Tem letra maiscula", a.isupper())
+print("É decimal?", a.isdecimal())
+
+
