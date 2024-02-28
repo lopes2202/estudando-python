9b08f9a8499d5bcf2fb5e885152c5aa1c73cb16d
Gabriel Lopes <gabriel.ldbrito@gmail.com>
2024-02-28 18:53:02 -0300

    modificando com .format

--- pythonteste.py
+++ pythonteste.py
@@ -1,9 +1,14 @@
 a = input('Digite algo: ')
 
-print("É um numero?", a.isnumeric())
-print("Faz parte do alfabeto", a.isalpha())
-print("Tem letra minuscula", a.islower())
-print("Tem letra maiscula", a.isupper())
-print("É decimal?", a.isdecimal())
+print("O tipo primitivo desse valor é:" , type(a))
+
+print("É um número? {}".format(a.isnumeric()))
+print("Faz parte do alfabeto? {}".format(a.isalpha()))
+print("Tem letra minuscula {}".format(a.islower()))
+print("Tem letra maiscula {}" .format(a.isupper()))
+print("Tem letra e numero {} " .format(a.isalnum()))
+print("É decimal {}".format(a.isdecimal()))
+print("Só tem espaços {}".format(a.isspace()))
+print("Está capitalizada {} " .format(a.istitle()))
 
 
