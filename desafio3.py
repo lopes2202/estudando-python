0290cfb9cbf802beb408b1ce3b295c1a3080fe15
Gabriel Lopes <gabriel.ldbrito@gmail.com>
2024-02-28 18:37:15 -0300

    modificando desafio3.py

--- desafio3.py
+++ desafio3.py
@@ -2,5 +2,4 @@ numero1 = int(input("Fale um numero "))
 numero2 = int(input("Fale outro numero "))
 
 soma = numero1 + numero2
-
-print("A soma dos números é:", soma)
+print('A soma entre {} e {} vale {}'.format(numero1, numero2, soma))
