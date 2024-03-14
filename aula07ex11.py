72107957f527c1998b8bb305a808052aca30cebf
Gabriel Lopes <137317230+lopes2202@users.noreply.github.com>
2024-03-13 21:31:25 -0300

    deletado, pois ja criei uma pasta pra as aulas, basicaamente botei os arquivo em ouitro lugar

--- aula07ex11.py
+++
@@ -1,5 +0,0 @@
-temperaturaC = float(input("Informe a temperatura em C°: "))
-
-temperaturaF = 9 * temperaturaC / 5 + 32
-
-print("A temperatura de {} C° corresponde a {} F!".format(temperaturaC, temperaturaF))
