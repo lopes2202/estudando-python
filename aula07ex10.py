a828a3a0e9edad70e47b65a11b4da0e91c98d27d
Gabriel Lopes <137317230+lopes2202@users.noreply.github.com>
2024-03-13 21:31:30 -0300

    deletado, pois ja criei uma pasta pra as aulas, basicaamente botei os arquivo em ouitro lugar

--- aula07ex10.py
+++
@@ -1,7 +0,0 @@
-produto = float(input("Digite o preço do produto: R$ "))
-
-pagamentoaVista = produto - (produto * 10/100)
-pagamentoParcelado = produto + (produto * 10/100)
-
-print("O produto custa {}R$ e pagando a vista tem um desconto de 10% e começará a custar: {}R$.".format(produto, pagamentoaVista))
-print("O produto custa {}R$ e pagando parcelado tem um aumento de 10% e começará a custar {}R$.".format(produto, pagamentoParcelado))
