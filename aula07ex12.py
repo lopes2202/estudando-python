df68195a770f98a567d68187e540996f3699a4ee
Gabriel Lopes <137317230+lopes2202@users.noreply.github.com>
2024-03-13 21:31:19 -0300

    deletado, pois ja criei uma pasta pra as aulas, basicaamente botei os arquivo em ouitro lugar

--- aula07ex12.py
+++
@@ -1,6 +0,0 @@
-diasAlugados = int(input("Quantos dias alugado? "))
-kmRodados = float(input("Quantos km rodados? "))
-
-totalPagamento = (diasAlugados * 60) + (kmRodados * 0.15)
-
-print("O total a pagar é de R${:.2f}" .format(totalPagamento))
